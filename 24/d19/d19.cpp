f2e1b90a7924e4d10e7c62212dd697bea81c75f6
Sean Carroll <seanwilliamcarroll@gmail.com>
2024-12-21 09:21:27 -0500

    d19 both parts

--- 24/d19/d19.cpp
+++ 24/d19/d19.cpp
@@ -1,10 +1,11 @@
+#include <algorithm> // for max_element
 #include <d19.hpp>
-#include <deque>     // for deque
 #include <fstream>   // for basic_istream, getline, basic_ifstream, basic_ios
-#include <set>       // for set
+#include <iostream>  // for cout
+#include <set>       // for set, operator!=, __tree_const_iterator
 #include <stddef.h>  // for size_t
 #include <stdexcept> // for runtime_error
-#include <string>    // for string, char_traits, basic_string, to_string
+#include <string>    // for char_traits, string, to_string, basic_string
 #include <utility>   // for make_pair, pair
 #include <vector>    // for vector
 
@@ -59,48 +60,72 @@ get_towel_patterns_and_towels_from_file(const std::string &filepath) {
   return std::make_pair(towel_patterns, towels);
 }
 
-bool is_design_possible(const TowelPatterns &towel_patterns,
-                        const Towel &towel) {
-  std::deque<size_t> positions;
-  positions.push_back(0);
+template <typename Element> void print_vector(const std::vector<Element> &vec) {
+  for (const auto &elem : vec) {
+    std::cout << elem << " ";
+  }
+  std::cout << std::endl;
+}
 
-  std::set<size_t> seen;
+size_t num_designs_possible(const std::set<TowelPattern> &unique_patterns,
+                            const size_t max_length_pattern,
+                            const Towel &towel) {
+  std::vector<size_t> counts(towel.size() + 1, 0);
+  counts[0] = 1;
 
-  while (!positions.empty()) {
-    size_t pos = positions.front();
-    positions.pop_front();
-    if (pos == towel.size()) {
-      return true;
-    }
-    if (seen.count(pos) > 0) {
-      continue;
-    }
-    for (const auto &pattern : towel_patterns) {
-      if (pattern.size() > towel.size() - pos) {
-        continue;
-      }
-      size_t index{};
-      while (index < pattern.size() && pattern[index] == towel[pos + index]) {
-        ++index;
-      }
-      if (index == pattern.size()) {
-        positions.push_back(pos + pattern.size());
+  for (size_t index = 1; index < towel.size() + 1; ++index) {
+    // Look at all previous pattern lengths, if the last n chars are a pattern,
+    // add to this count
+    for (size_t length = 1; length <= max_length_pattern && length <= index;
+         ++length) {
+      const auto sub_string = towel.substr(index - length, length);
+      if (unique_patterns.count(sub_string) > 0) {
+        counts[index] += counts[index - length];
       }
     }
-    seen.insert(pos);
   }
 
-  return false;
+  return counts.back();
 }
 
 size_t get_number_possible_designs(const TowelPatterns &towel_patterns,
                                    const Towels &towels) {
   size_t number_possible{};
 
+  const std::set<TowelPattern> unique_patterns(towel_patterns.begin(),
+                                               towel_patterns.end());
+  std::set<size_t> pattern_lengths;
+  for (const auto &pattern : towel_patterns) {
+    pattern_lengths.insert(pattern.size());
+  }
+  const size_t max_length_pattern =
+      *std::max_element(pattern_lengths.begin(), pattern_lengths.end());
+
   for (const auto &towel : towels) {
-    if (is_design_possible(towel_patterns, towel)) {
+    if (num_designs_possible(unique_patterns, max_length_pattern, towel) > 0) {
       ++number_possible;
     }
+  }
+
+  return number_possible;
+}
+
+size_t get_total_number_possible_designs(const TowelPatterns &towel_patterns,
+                                         const Towels &towels) {
+  size_t number_possible{};
+
+  const std::set<TowelPattern> unique_patterns(towel_patterns.begin(),
+                                               towel_patterns.end());
+  std::set<size_t> pattern_lengths;
+  for (const auto &pattern : towel_patterns) {
+    pattern_lengths.insert(pattern.size());
+  }
+  const size_t max_length_pattern =
+      *std::max_element(pattern_lengths.begin(), pattern_lengths.end());
+
+  for (const auto &towel : towels) {
+    number_possible +=
+        num_designs_possible(unique_patterns, max_length_pattern, towel);
   }
 
   return number_possible;
@@ -116,6 +141,15 @@ std::string part_1(const std::string &filepath) {
   return std::to_string(num_possible);
 }
 
-std::string part_2(const std::string &filepath) { return std::string(); }
+std::string part_2(const std::string &filepath) {
+
+  const auto [towel_patterns, towels] =
+      get_towel_patterns_and_towels_from_file(filepath);
+
+  const auto num_possible =
+      get_total_number_possible_designs(towel_patterns, towels);
+
+  return std::to_string(num_possible);
+}
 
 } // namespace d19
