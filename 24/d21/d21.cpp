bf7d86cf0ee4cd960aaf897b9384c791d7acfa75
Sean Carroll <seanwilliamcarroll@gmail.com>
2024-12-22 15:30:43 -0500

    d21 both parts, needed help with dynamic programming solution

--- 24/d21/d21.cpp
+++ 24/d21/d21.cpp
@@ -1,8 +1,8 @@
 #include <core_lib.hpp> // for Position, Tile, get_lines_from_file
 #include <d21.hpp>
-#include <map>      // for map
+#include <map>      // for map, __map_const_iterator, operator!=
 #include <stddef.h> // for size_t
-#include <string>   // for basic_string, allocator, operator+, char_traits
+#include <string>   // for operator+, basic_string, string, to_string
 #include <utility>  // for pair, make_pair, operator==
 #include <vector>   // for vector
 
@@ -13,23 +13,15 @@ namespace d21 {
 using Sequence = std::string;
 using Sequences = std::vector<Sequence>;
 
-constexpr Tile INVALID = 99;
+using Movement = std::pair<Position, Position>;
+using MemoizationKey = std::pair<Movement, size_t>;
 
-// constexpr size_t NUMBER_PAD_ROWS = 4;
-// constexpr size_t NUMBER_PAD_COLS = 3;
-// constexpr std::array<std::array<Tile, NUMBER_PAD_COLS>, NUMBER_PAD_ROWS>
-//     NUMBER_PAD = {
-//   {{'7', '8', '9'}, {'4', '5', '6'}, {'1', '2', '3'}, {INVALID, '0', 'A'}}};
+constexpr Tile INVALID = 99;
 
 const std::map<Tile, Position> NUMBER_PAD_POSITIONS = {
     {'7', {0, 0}}, {'8', {0, 1}},     {'9', {0, 2}}, {'4', {1, 0}},
     {'5', {1, 1}}, {'6', {1, 2}},     {'1', {2, 0}}, {'2', {2, 1}},
     {'3', {2, 2}}, {INVALID, {3, 0}}, {'0', {3, 1}}, {'A', {3, 2}}};
-
-// constexpr size_t DIRECTION_PAD_ROWS = 2;
-// constexpr size_t DIRECTION_PAD_COLS = 3;
-// constexpr std::array<std::array<Tile, DIRECTION_PAD_COLS>,
-// DIRECTION_PAD_ROWS> DIRECTION_PAD = {{{INVALID, '^', 'A'}, {'<', 'v', '>'}}};
 
 const std::map<Tile, Position> DIRECTION_PAD_POSITIONS = {{{INVALID, {0, 0}},
                                                            {'^', {0, 1}},
@@ -44,10 +36,9 @@ constexpr Sequence ENTER_SEQUENCE = "A";
 const std::map<Tile, Position> DIRECTION_INCREMENTS = {
     {{'^', {-1, 0}}, {'>', {0, 1}}, {'<', {0, -1}}, {'v', {1, 0}}}};
 
-std::pair<Sequence, Position>
-get_sequence(const Position &initial_position, const Tile target_tile,
-             const std::map<Tile, Position> &pad_positions) {
-  const Position target_position = pad_positions.at(target_tile);
+Sequence get_sequence(const Position &initial_position,
+                      const Position &target_position,
+                      const std::map<Tile, Position> &pad_positions) {
   const auto &[target_row, target_col] = target_position;
   const auto &[initial_row, initial_col] = initial_position;
 
@@ -58,26 +49,18 @@ get_sequence(const Position &initial_position, const Tile target_tile,
 
   // Do we only need to move in one direction
   if (target_row == initial_row && target_col == initial_col) {
-    return std::make_pair(ENTER_SEQUENCE, target_position);
+    return ENTER_SEQUENCE;
   } else if (target_row == initial_row) {
     if (initial_col < target_col) {
-      return std::make_pair(Sequence(target_col - initial_col, '>') +
-                                ENTER_SEQUENCE,
-                            target_position);
+      return Sequence(target_col - initial_col, '>') + ENTER_SEQUENCE;
     } else {
-      return std::make_pair(Sequence(initial_col - target_col, '<') +
-                                ENTER_SEQUENCE,
-                            target_position);
+      return Sequence(initial_col - target_col, '<') + ENTER_SEQUENCE;
     }
   } else if (target_col == initial_col) {
     if (initial_row < target_row) {
-      return std::make_pair(Sequence(target_row - initial_row, 'v') +
-                                ENTER_SEQUENCE,
-                            target_position);
+      return Sequence(target_row - initial_row, 'v') + ENTER_SEQUENCE;
     } else {
-      return std::make_pair(Sequence(initial_row - target_row, '^') +
-                                ENTER_SEQUENCE,
-                            target_position);
+      return Sequence(initial_row - target_row, '^') + ENTER_SEQUENCE;
     }
   }
   // We need to do both directions
@@ -97,60 +80,51 @@ get_sequence(const Position &initial_position, const Tile target_tile,
   // Will we hit invalid?
   if (std::make_pair(initial_row, target_col) == invalid_position) {
     // Going vertical first will hit invalid
-    return std::make_pair(horizontal + vertical + ENTER_SEQUENCE,
-                          target_position);
+    return horizontal + vertical + ENTER_SEQUENCE;
   } else if (std::make_pair(target_row, initial_col) == invalid_position) {
     // Going horizontal first will hit invalid
-    return std::make_pair(vertical + horizontal + ENTER_SEQUENCE,
-                          target_position);
+    return vertical + horizontal + ENTER_SEQUENCE;
   }
 
   if (initial_row > target_row && initial_col > target_col) {
-    return std::make_pair(vertical + horizontal + ENTER_SEQUENCE,
-                          target_position);
+    return vertical + horizontal + ENTER_SEQUENCE;
   } else if (initial_row > target_row && initial_col < target_col) {
-    return std::make_pair(horizontal + vertical + ENTER_SEQUENCE,
-                          target_position);
+    return horizontal + vertical + ENTER_SEQUENCE;
   } else if (initial_row < target_row && initial_col > target_col) {
-    return std::make_pair(vertical + horizontal + ENTER_SEQUENCE,
-                          target_position);
+    return vertical + horizontal + ENTER_SEQUENCE;
   } else { // if (initial_row < target_row && initial_col < target_col) {
-    return std::make_pair(horizontal + vertical + ENTER_SEQUENCE,
-                          target_position);
+    return horizontal + vertical + ENTER_SEQUENCE;
   }
 }
 
-Sequence get_full_sequence(const Sequence &sequence,
-                           const std::map<Tile, Position> &pad_positions) {
-  auto position = pad_positions.at(ENTER_KEY);
-  Sequence output;
-  for (const auto tile : sequence) {
-    const auto [sub_sequence, next_position] =
-        get_sequence(position, tile, pad_positions);
-    output += sub_sequence;
-    position = next_position;
+std::map<Movement, Sequence>
+calc_lookup_table(const std::map<Tile, Position> &pad_positions) {
+  std::map<Movement, Sequence> lookup_table;
+  for (const auto &[start_tile, start_position] : pad_positions) {
+    if (start_tile == INVALID) {
+      continue;
+    }
+    for (const auto &[target_tile, target_position] : pad_positions) {
+      if (target_tile == INVALID) {
+        continue;
+      }
+      const auto movement = std::make_pair(start_position, target_position);
+      if (start_position == target_position) {
+        lookup_table[movement] = ENTER_SEQUENCE;
+        continue;
+      }
+      lookup_table[movement] =
+          get_sequence(start_position, target_position, pad_positions);
+    }
   }
-  return output;
+  return lookup_table;
 }
 
-Sequence get_complete_sequence(const Sequence &sequence,
-                               const size_t number_robots) {
-  const Sequence numeric_sequence =
-      get_full_sequence(sequence, NUMBER_PAD_POSITIONS);
-  // std::cout << numeric_sequence << std::endl;
+const auto NUMBER_MOVEMENT_TO_SEQUENCE =
+    calc_lookup_table(NUMBER_PAD_POSITIONS);
 
-  Sequence robot_sequence = numeric_sequence;
-  for (size_t index{}; index < number_robots - 1; ++index) {
-    robot_sequence = get_full_sequence(robot_sequence, DIRECTION_PAD_POSITIONS);
-  }
-  // std::cout << robot_sequence << std::endl;
-
-  const Sequence human_sequence =
-      get_full_sequence(robot_sequence, DIRECTION_PAD_POSITIONS);
-  // std::cout << human_sequence << std::endl;
-
-  return human_sequence;
-}
+const auto DIRECTION_MOVEMENT_TO_SEQUENCE =
+    calc_lookup_table(DIRECTION_PAD_POSITIONS);
 
 size_t get_numeric_part(const Sequence &sequence) {
   size_t output{};
@@ -164,30 +138,79 @@ size_t get_numeric_part(const Sequence &sequence) {
   return output;
 }
 
+std::map<MemoizationKey, size_t> previous_lengths;
+
+size_t calc_length_at_level(const Position &start_position,
+                            const Position &target_position,
+                            const size_t num_robots) {
+
+  const auto movement = std::make_pair(start_position, target_position);
+  const auto key = std::make_pair(movement, num_robots);
+
+  const auto &iter = previous_lengths.find(key);
+  if (iter != previous_lengths.end()) {
+    return iter->second;
+  }
+
+  if (num_robots == 0) {
+    const auto output = DIRECTION_MOVEMENT_TO_SEQUENCE.at(movement).size();
+    previous_lengths[key] = output;
+    return output;
+  }
+  const auto sequence = DIRECTION_MOVEMENT_TO_SEQUENCE.at(movement);
+  size_t sum{};
+  auto last_position = DIRECTION_PAD_POSITIONS.at(ENTER_KEY);
+  for (const auto tile : sequence) {
+    auto next_position = DIRECTION_PAD_POSITIONS.at(tile);
+    sum += calc_length_at_level(last_position, next_position, num_robots - 1);
+    last_position = next_position;
+  }
+  previous_lengths[key] = sum;
+  return sum;
+}
+
+size_t get_total_length(const Sequence &sequence, size_t num_robots) {
+  auto last_position = NUMBER_PAD_POSITIONS.at(ENTER_KEY);
+  size_t sum{};
+  auto last_dir_position = DIRECTION_PAD_POSITIONS.at(ENTER_KEY);
+  for (const auto tile : sequence) {
+    auto next_position = NUMBER_PAD_POSITIONS.at(tile);
+    auto movement = std::make_pair(last_position, next_position);
+    const auto number_sequence = NUMBER_MOVEMENT_TO_SEQUENCE.at(movement);
+    for (const auto sequence_tile : number_sequence) {
+      auto next_dir_position = DIRECTION_PAD_POSITIONS.at(sequence_tile);
+      sum += calc_length_at_level(last_dir_position, next_dir_position,
+                                  num_robots - 1);
+      last_dir_position = next_dir_position;
+    }
+    last_position = next_position;
+  }
+  return sum;
+}
+
 std::string part_1(const std::string &filepath) {
-
   const auto sequences = get_lines_from_file(filepath);
-
-  // print_lines(sequences);
-
-  // const Position initial_position = NUMBER_PAD_POSITIONS.at('A');
-
-  // const Position target_position = NUMBER_PAD_POSITIONS.at('7');
 
   size_t complexity{};
   for (const auto &sequence : sequences) {
-    const Sequence output_sequence = get_complete_sequence(sequence, 2);
-    // std::cout << sequence << std::endl;
-    // std::cout << output_sequence << std::endl;
-    // std::cout << "Length: " << output_sequence.size() << std::endl;
-    // std::cout << "Numeric Part: " << get_numeric_part(sequence) << std::endl;
-    complexity += (output_sequence.size() * get_numeric_part(sequence));
+    const size_t length = get_total_length(sequence, 2);
+    complexity += (length * get_numeric_part(sequence));
   }
 
   return std::to_string(complexity);
 }
 
-std::string part_2(const std::string &filepath) { return std::string(); }
+std::string part_2(const std::string &filepath) {
+  const auto sequences = get_lines_from_file(filepath);
+
+  size_t complexity{};
+  for (const auto &sequence : sequences) {
+    const size_t length = get_total_length(sequence, 25);
+    complexity += (length * get_numeric_part(sequence));
+  }
+
+  return std::to_string(complexity);
+}
 
 // clang-format off
 } // namespace d21
