175c5cbcdb9df4ba3e764b478cf45aaeedce566d
Sean Carroll <seanwilliamcarroll@gmail.com>
2024-12-22 14:11:26 -0500

    d22 second part, sorta slow

--- 24/d22/d22.cpp
+++ 24/d22/d22.cpp
@@ -1,13 +1,17 @@
 #include <d22.hpp>
 #include <fstream>  // for basic_istream, basic_ifstream, ifstream
+#include <map>      // for map
+#include <set>      // for set, __tree_const_iterator
 #include <stddef.h> // for size_t
+#include <tuple>    // for make_tuple, tuple
+#include <utility>  // for make_pair, pair
 #include <vector>   // for vector
 
 // clang-format off
 namespace d22 {
 // clang-format on
 
-using Value = unsigned long long;
+using Value = long long;
 
 using Values = std::vector<Value>;
 
@@ -68,13 +72,68 @@ Value get_total_sum_of_2000th_secret_number(const Values &values) {
   return output;
 }
 
+std::pair<Values, Values>
+calc_2000_secret_numbers_and_change_sequence(const Value value) {
+  Values prices;
+  prices.reserve(2000);
+  Values changes;
+  changes.reserve(2000);
+  Value last_value = value;
+  Value last_price = value % 10;
+  for (size_t index{}; index < 2000; ++index) {
+    Value next_value = generate_next_secret_number(last_value);
+    Value next_price = next_value % 10;
+    prices.push_back(next_price);
+    changes.push_back(next_price - last_price);
+    last_value = next_value;
+    last_price = next_price;
+  }
+  return std::make_pair(prices, changes);
+}
+
+Value get_most_bananas(const Values values) {
+  using PriceChangeSequence = std::tuple<Value, Value, Value, Value>;
+  std::set<PriceChangeSequence> all_sequences;
+  std::map<PriceChangeSequence, Value> first_values;
+  std::set<PriceChangeSequence> seen;
+  for (const auto value : values) {
+    const auto [prices, changes] =
+        calc_2000_secret_numbers_and_change_sequence(value);
+    seen.clear();
+    for (size_t index = 3; index < changes.size(); ++index) {
+      PriceChangeSequence sequence =
+          std::make_tuple(changes[index - 3], changes[index - 2],
+                          changes[index - 1], changes[index]);
+      if (seen.count(sequence) > 0) {
+        continue;
+      }
+      seen.insert(sequence);
+      all_sequences.insert(sequence);
+      first_values[sequence] += prices[index];
+    }
+  }
+
+  Value most_bananas{};
+  for (const auto &sequence : all_sequences) {
+    Value banana_count = first_values[sequence];
+    if (banana_count > most_bananas) {
+      most_bananas = banana_count;
+    }
+  }
+  return most_bananas;
+}
+
 std::string part_1(const std::string &filepath) {
   const auto values = get_values_from_file(filepath);
 
   return std::to_string(get_total_sum_of_2000th_secret_number(values));
 }
 
-std::string part_2(const std::string &filepath) { return std::string(); }
+std::string part_2(const std::string &filepath) {
+  const auto values = get_values_from_file(filepath);
+
+  return std::to_string(get_most_bananas(values));
+}
 
 // clang-format off
 } // namespace d22
