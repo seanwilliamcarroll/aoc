8a076fd9d3ba0f39079fbcd893176fa331d32bba
Sean Carroll <seanwilliamcarroll@gmail.com>
2024-12-23 15:03:57 -0500

    d23 both parts

--- 24/d23/d23.cpp
+++ 24/d23/d23.cpp
@@ -1,10 +1,13 @@
 #include <algorithm> // for sort
 #include <d23.hpp>
+#include <deque>    // for deque
 #include <fstream>  // for basic_istream, basic_ifstream, getline, ifstream
+#include <iterator> // for next
 #include <map>      // for map
-#include <set>      // for set, __tree_const_iterator, operator!=
+#include <numeric>  // for accumulate
+#include <set>      // for set, operator!=, __tree_const_iterator
 #include <stddef.h> // for size_t
-#include <string>   // for char_traits, string, basic_string, to_string
+#include <string>   // for allocator, char_traits, string, basic_string
 #include <tuple>    // for make_tuple, tuple
 #include <utility>  // for pair
 #include <vector>   // for vector
@@ -103,6 +106,82 @@ ThreeSets find_all_three_sets_starts_with(const Network &network,
   return three_sets_starts_with;
 }
 
+ConnectedGroups find_all_cliques(const Network &network) {
+
+  // Want to find every group of computers that is connected to all other
+  // computers
+
+  ConnectedGroups cliques;
+  ConnectedGroup in_clique;
+
+  for (const auto &computer : network.m_computers) {
+    // For each computer that we're connected to, check if they are all
+    // connected to one another?
+    if (in_clique.count(computer) > 0) {
+      // Already found the correct clique
+      continue;
+    }
+    for (const auto &adjacent_computer : network.m_edges.at(computer)) {
+      if (in_clique.count(adjacent_computer) > 0) {
+        // Already found the correct clique
+        continue;
+      }
+      // Now found at least two computers not already in a clique
+      ConnectedGroup current_clique{computer, adjacent_computer};
+      std::deque<Computer> computers_to_try;
+      for (const auto &clique_computer : current_clique) {
+        const auto &adjacent_computers = network.m_edges.at(clique_computer);
+        computers_to_try.insert(computers_to_try.end(),
+                                adjacent_computers.begin(),
+                                adjacent_computers.end());
+      }
+      while (!computers_to_try.empty()) {
+        const auto computer_to_try = computers_to_try.front();
+        computers_to_try.pop_front();
+        if (current_clique.count(computer_to_try) > 0) {
+          continue;
+        }
+        // Check against all computers in clique
+        bool all_connected = true;
+        for (const auto &clique_computer : current_clique) {
+          all_connected &=
+              network.m_edges.at(clique_computer).count(computer_to_try) > 0;
+          if (!all_connected) {
+            break;
+          }
+        }
+        if (!all_connected) {
+          continue;
+        }
+        current_clique.insert(computer_to_try);
+        // Add the adjacent ones to computers_to_try
+        const auto &potential_computers = network.m_edges.at(computer_to_try);
+        computers_to_try.insert(computers_to_try.end(),
+                                potential_computers.begin(),
+                                potential_computers.end());
+      }
+      // Cycle through the clique and add each lookup to
+      in_clique.insert(current_clique.begin(), current_clique.end());
+      cliques.insert(current_clique);
+    }
+  }
+  return cliques;
+}
+
+ConnectedGroup find_largest_clique(const Network &network) {
+  const ConnectedGroups all_cliques = find_all_cliques(network);
+
+  size_t largest{};
+  ConnectedGroup largest_clique;
+  for (const auto &clique : all_cliques) {
+    if (clique.size() > largest) {
+      largest = clique.size();
+      largest_clique = clique;
+    }
+  }
+  return largest_clique;
+}
+
 std::string part_1(const std::string &filepath) {
 
   const auto connections = get_connections_from_file(filepath);
@@ -114,7 +193,23 @@ std::string part_1(const std::string &filepath) {
   return std::to_string(three_sets.size());
 }
 
-std::string part_2(const std::string &filepath) { return std::string(); }
+std::string part_2(const std::string &filepath) {
+
+  const auto connections = get_connections_from_file(filepath);
+
+  const auto network = create_network_from_connections(connections);
+
+  const auto largest_clique = find_largest_clique(network);
+
+  Computers clique(largest_clique.begin(), largest_clique.end());
+  std::sort(clique.begin(), clique.end());
+  const std::string clique_names =
+      std::accumulate(std::next(clique.begin()), clique.end(), clique.front(),
+                      [](const std::string &left, const std::string &right) {
+                        return left + std::string(",") + right;
+                      });
+  return clique_names;
+}
 
 // clang-format off
 } // namespace d23
