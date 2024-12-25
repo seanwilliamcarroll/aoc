17fb231a97bcc5463c8fce3ef992b36de7529017
Sean Carroll <seanwilliamcarroll@gmail.com>
2024-12-25 12:21:16 -0500

    formatting

--- 24/_template/d{{cookiecutter.day_id}}/d{{cookiecutter.day_id}}.cpp
+++ 24/_template/d{{cookiecutter.day_id}}/d{{cookiecutter.day_id}}.cpp
@@ -4,9 +4,13 @@
 namespace d{{cookiecutter.day_id}} {
   // clang-format on
 
-  std::string part_1(const std::string &filepath) { return std::string(); }
+  std::string part_1(const std::string &filepath) {
+    return std::string();
+  }
 
-  std::string part_2(const std::string &filepath) { return std::string(); }
+  std::string part_2(const std::string &filepath) {
+    return std::string();
+  }
 
   // clang-format off
 } // namespace d{{cookiecutter.day_id}}
