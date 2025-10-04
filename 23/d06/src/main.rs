31c01af121795d8e29b9defa6995a60b8a6f11dd
Sean Carroll <seanwilliamcarroll@gmail.com>
2025-10-04 15:07:04 -0400

    23d6 both parts algebra

--- 23/d06/src/main.rs
+++ 23/d06/src/main.rs
@@ -17,12 +17,42 @@ fn parse_numbers(line: &str, initial_string: &str) -> Vec<usize> {
         .collect::<Vec<usize>>()
 }
 
-fn simulate_scores(time: usize) -> Vec<usize> {
-    let mut output = vec![];
-    for held in 0..=time {
-        output.push((time - held) * held);
+fn parse_as_one_number(line: &str, initial_string: &str) -> usize {
+    line.trim_start_matches(initial_string)
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>()
+        .parse::<usize>()
+        .expect("Should parse")
+}
+
+fn calculate_score(time: usize, hold_time: usize) -> usize {
+    (time - hold_time) * hold_time
+}
+
+fn find_greater(value: f64, time: usize, distance: usize) -> usize {
+    let floor = value.floor() as usize;
+    let ceil = value.ceil() as usize;
+    if calculate_score(time, floor) > distance {
+        floor
+    } else {
+        assert!(calculate_score(time, ceil) > distance);
+        ceil
     }
-    output
+}
+
+fn find_roots(a: f64, b: f64, c: f64) -> (f64, f64) {
+    let sqrt_term = (b.powf(2f64) - (4f64 * a * c)).sqrt();
+    let first = (-b + sqrt_term) / (2f64 * a);
+    let second = (-b - sqrt_term) / (2f64 * a);
+    (first, second)
+}
+
+fn find_num_greater(time: usize, distance: usize) -> usize {
+    let (first_root, second_root) = find_roots(-1f64, time as f64, -(distance as f64));
+    let first_greatest = find_greater(first_root, time, distance);
+    let second_greatest = find_greater(second_root, time, distance);
+    second_greatest - first_greatest + 1
 }
 
 fn main() -> std::io::Result<()> {
@@ -43,18 +73,21 @@ fn main() -> std::io::Result<()> {
 
     let mut p1_out = 1usize;
     for (time, distance) in list {
-        println!("In time {time}, cover {distance}");
-        let scores = simulate_scores(time);
-        let better = scores
-            .iter()
-            .filter(|score| **score > distance)
-            .cloned()
-            .collect::<Vec<usize>>()
-            .len();
-        p1_out *= better;
+        p1_out *= find_num_greater(time, distance);
     }
 
     println!("P1 output: {p1_out}");
+
+    let single_time = parse_as_one_number(&raw_lines[0], "Time: ");
+    let single_distance = parse_as_one_number(&raw_lines[1], "Distance: ");
+
+    println!("Time:     {single_time:>15}");
+    println!("Distance: {single_distance:>15}");
+
+    println!(
+        "P2 number of ways: {}",
+        find_num_greater(single_time, single_distance)
+    );
 
     Ok(())
 }
